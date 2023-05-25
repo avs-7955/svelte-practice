d834d7b28ea27ef41e688b902c87bc9d6dfe92b8
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-25 12:52:44 +0530

    initial commit, basic str

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Modal from "./Modal.svelte"
+	let people = [
+		{ name: "yoshi", beltColour: "black", age: 25, id: 1 },
+		{ name: "mario", beltColour: "orange", age: 45, id: 2 },
+		{ name: "luigi", beltColour: "brown", age: 35, id: 3 },
+	]
+
+	let showModal = false
+
+	const toggleModal = () => {
+		showModal = !showModal
+	}
+
+	const handleClick = (id) => {
+		people = people.filter((person) => person.id != id)
+	}
+</script>
+
+<Modal
+	message="Hey! I am a prop value."
+	isPromo={false}
+	{showModal}
+	on:click={toggleModal}
+/>
+<main>
+	<button on:click|once={toggleModal}>Show Modal</button>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColour === "black"}
+				<p><strong>MASTER NINJA</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+			<button on:click={() => handleClick(person.id)}>Delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let isPromo = false
+	export let showModal = false
+	export let message = "default message"
+</script>
+
+{#if showModal}
+	<div class="backdrop" class:promo={isPromo} on:click|self>
+		<div class="modal" />
+		<p>{message}</p>
+	</div>
+{/if}
+
+<style>
+	.backdrop {
+		background-color: rgba(0, 0, 0, 0.8);
+		width: 100%;
+		height: 100%;
+		position: fixed;
+	}
+	.modal {
+		background: white;
+		width: 70%;
+		text-align: center;
+		padding: 10px;
+		border-radius: 15px;
+		max-width: 400px;
+		margin: 10% auto;
+	}
+	.promo .modal {
+		background: crimson;
+		color: white;
+	}
+</style>
